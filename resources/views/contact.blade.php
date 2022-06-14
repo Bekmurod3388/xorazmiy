e725469e765625dcf7e2c5e99c2446ce793b36cb
bekmurod <bekmurod3388@gmail.com>
2022-06-14 12:30:33 +0500

    phone number changed

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -91,7 +91,7 @@
                                         <i class="fa fa-phone"></i>
                                     </div>
                                     <div class="cont">
-                                        <p>+998-(93)-453-28-27</p>
+                                        <p>+998-(97)-453-28-27</p>
                                     </div>
                                 </div> <!-- singel address -->
                             </li>
