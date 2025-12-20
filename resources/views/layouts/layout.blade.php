d69d8a55fca00c46aaebf5f5e3ca398e61a42334
YFayzulla <yoldashevfayzulla0@gmail.com>
2025-12-20 09:18:41 +0500

    bug fix

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -49,6 +49,8 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/limonte-sweetalert2/6.6.9/sweetalert2.min.js"></script>
     <script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
+    @yield('css')
+
 </head>
 
 <body>
--- resources/views/photo.blade.php
+++ resources/views/photo.blade.php
@@ -1,14 +1,36 @@
 @extends('layouts.layout')
+@section('css')
+<style>
+.gallery {
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    gap: 20px;
+    }
+
+    .gallery-item {
+    width: 100%;
+    }
+
+    .gallery-item img {
+    width: 100%;
+    height: auto;
+    border-radius: 12px;
+    object-fit: cover;
+    }
+</style>
+@endsection
 @section('content')
-<div class="container">
-    @foreach($photos as $photo)
-    <div class="gallery" id="gallery">
-    <div class="gallery-item">
-        <div class="content"><img src="{{asset('/storage/photo/' . $photo->image)}}" alt=""></div>
+    <div class="container">
+        <div class="gallery" id="gallery">
+            @foreach($photos as $photo)
+                <div class="gallery-item">
+                    <div class="content">
+                        <img src="{{ asset('/storage/photo/' . $photo->image) }}" alt="">
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 
-        @endforeach
-</div>
-    <script src="{{asset('fasset/js/gallery.js')}}"></script>
-
+    <script src="{{ asset('fasset/js/gallery.js') }}"></script>
 @endsection
